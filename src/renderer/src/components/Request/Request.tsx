129ac37f3f615483333ffe9b7737a448bcb9b2a2
jerry <jerrynwosu007@gmail.com>
2023-07-14 02:05:31 +0100

    update

--- src/renderer/src/components/Request/Request.tsx
+++ src/renderer/src/components/Request/Request.tsx
@@ -1,11 +1,20 @@
-import { Text } from '@mantine/core'
+import { Button, Select, TextInput } from '@mantine/core'
+import styles from './Request.module.css'
+
+const methods = [
+  { value: 'get', label: 'GET' },
+  { value: 'post', label: 'POST' },
+  { value: 'put', label: 'PUT' }
+]
 
 export default function Request(): JSX.Element {
   return (
-    <div>
-      <div>
-        <Text>This is from Request...</Text>
-      </div>
+    <div className={styles.request_wrapper}>
+      <Select defaultValue="get" data={methods} searchable radius={0} />
+      <form className={styles.request_form}>
+        <TextInput className={styles.request_input} radius={0} />
+        <Button>Send</Button>
+      </form>
     </div>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -6,7 +6,7 @@ import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
       <App />
     </MantineProvider>
   </React.StrictMode>
